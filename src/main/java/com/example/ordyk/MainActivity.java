9c51a0897fcd450cae57855c0931ea9470959adf
BlakeGrevelson <139565008+BlakeGrevelson@users.noreply.github.com>
2024-05-27 21:36:49 +0700

    first commit

---
+++ src/main/java/com/example/ordyk/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.ordyk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnSignIn, btnSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(MainActivity.this, SignIn.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
